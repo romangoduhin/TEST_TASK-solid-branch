016eab3169e930aab9ea34422109cffbdeeac063
romangoduhin <superchewits2000@gmail.com>
2023-05-15 15:01:11 +0300

    fix: update widthByCount enum to object, fix incorrect its values

--- my-app/src/components/List/Table/Row/Row.tsx
+++ my-app/src/components/List/Table/Row/Row.tsx
@@ -2,20 +2,20 @@ import React from "react";
 import { IRowProps } from "./Row.types";
 import { Cell } from "./Cell";
 
-enum widthByCount {
-  "w-full" = 1,
-  "w-1/2",
-  "w-1/3",
-  "w-1/4",
-  "w-1/6",
-  "w-1/7",
-  "w-1/8",
-  "w-1/9",
-}
+const widthVariants = {
+  1: "w-full",
+  2: "w-1/2",
+  3: "w-1/3",
+  4: "w-1/4",
+  5: "w-1/5",
+  6: "w-1/6",
+  7: "w-1/7",
+  8: "w-1/8",
+};
 
 export const Row: React.FC<IRowProps> = ({ className, data }) => {
   const columnsCount = 2;
-  const cellWidth = widthByCount[columnsCount];
+  const cellWidth = widthVariants[columnsCount];
 
   return (
     <div className={`${className} w-full h-[10%] flex flex-row border-[1px] bg-white hover:bg-gray-light`}>
